3e71bc8efe86fd1f6a5c093875effbb5b893951f
akhmanova <eakhmanova@gmail.com>
2017-02-03 17:18:00 +0400

    Update geolocation.sh

--- geolocation.sh
+++ geolocation.sh
@@ -1,3 +1,3 @@
 POST_DATA='{"considerIp": "false", "wifiAccessPoints": [ { "macAddress": "00:25:9c:cf:1c:ac", "signalStrength": -43, "signalToNoiseRatio": 0}, { "macAddress":"00:25:9c:cf:1c:ad", "signalStrength": -55, "signalToNoiseRatio": 0 }]}'
 
-wget -O "/home/eakhmanova/git_gd/geolocation.json" --post-data "$POST_DATA" --header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDIcQLFKJrvON96_G76_RATwjBk51XunLc"
+wget -O "/home/eakhmanova/git_gd/storing-routers-gelolocation/geolocation.json" --post-data "$POST_DATA" --header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDIcQLFKJrvON96_G76_RATwjBk51XunLc"
