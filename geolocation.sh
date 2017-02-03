e2eff7022b7aca51f05d9afc327e7c3bbf2ce41d
akhmanova <eakhmanova@gmail.com>
2017-02-03 16:55:16 +0400

    Update geolocation.sh

--- geolocation.sh
+++ geolocation.sh
@@ -1,7 +1,5 @@
 $GOOGLE_KEY="$AIzaSyDIcQLFKJrvON96_G76_RATwjBk51XunLc"
 
-$OUR_DIR="/home/eakhmanova/git_gd"
-
 $POST_DATA='{"considerIp": "false", "wifiAccessPoints": [ { "macAddress": "00:25:9c:cf:1c:ac", "signalStrength": -43, "signalToNoiseRatio": 0}, { "macAddress":"00:25:9c:cf:1c:ad", "signalStrength": -55, "signalToNoiseRatio": 0 }]}'
 
-wget -O "$OUR_DIR/geolocation.json" —post-data "$POST_DATA" —header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=$GOOGLE_KEY"
+wget -O "/home/eakhmanova/git_gd/geolocation.json" —post-data "$POST_DATA" —header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDIcQLFKJrvON96_G76_RATwjBk51XunLc"
