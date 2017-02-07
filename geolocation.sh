9fd48d1f88d6de9d6914625204a05bdee61b4520
akhmanova <eakhmanova@gmail.com>
2017-02-07 19:27:13 +0400

    SignalLevel changed

--- geolocation.sh
+++ geolocation.sh
@@ -1,6 +1,6 @@
 POST_DATA='{"considerIp": "false", "wifiAccessPoints": [ { "macAddress": "00:25:9c:cf:1c:ac", "signalStrength": -43, "signalToNoiseRatio": 0}, { "macAddress":"00:25:9c:cf:1c:ad", "signalStrength": -55, "signalToNoiseRatio": 0 }]}'
 
-wget -O "/home/eakhmanova/git_gd/storing-routers-geolocation/geolocation.json" --post-data "$POST_DATA" --header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDIcQLFKJrvON96_G76_RATwjBk51XunLc"
+wget -O "/home/eakhmanova/git_gd/storing-routers-geolocation/geolocation.json" --post-data "$POST_DATA" --header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDIcHACKMEPLZ_RATwjBk51XunLc"
 
 git add geolocation.json
 
