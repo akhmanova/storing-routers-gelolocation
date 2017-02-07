956aa3c36a73bd9289c34e032c12cc8b8ddb892c
akhmanova <eakhmanova@gmail.com>
2017-02-07 19:25:57 +0400

    SignalLevel changed

--- act.sh
+++ act.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
 N=1
-cat iwlist_example.log | awk '/Address/ {print $5}'| while read AP
+awk '/Address/ {print $5}' iwlist_example.log  
+while read AP
 do
 AccessPoints[$N]=$AP
 N=$((N+1))
 done
+N=1
+cat grep "Signal level" iwlist_example.log | sed -e 's/^.*Signal level=\(.*\) dBm/\1/'  
+while read SL
+do
+SignalLevel[$N]=$SL
+N=$((N+1))
+done
+
