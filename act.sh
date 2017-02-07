1ce29baaca36c5ed4fca2a42d1099256b5fef158
akhmanova <eakhmanova@gmail.com>
2017-02-07 19:40:50 +0400

    while changed

--- act.sh
+++ act.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 N=1
-awk '/Address/ {print $5}' iwlist_example.log  
-while read AP
+awk '/Address/ {print $5}' iwlist_example.log  | while read AP
 do
 AccessPoints[$N]=$AP
 N=$((N+1))
 done
 N=1
-cat grep "Signal level" iwlist_example.log | sed -e 's/^.*Signal level=\(.*\) dBm/\1/'  
-while read SL
+cat grep "Signal level" iwlist_example.log | sed -e 's/^.*Signal level=\(.*\) dBm/\1/'  | while read SL
 do
 SignalLevel[$N]=$SL
 N=$((N+1))
