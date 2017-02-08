454934e2a7de3bcedf4e49481505c5a48b504343
akhmanova <eakhmanova@gmail.com>
2017-02-08 13:17:42 +0400

    Bags fixed

--- act.sh
+++ act.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 N=1
-awk '/Address/ {print $5}' iwlist_example.log  | while read AP
+while read AP
 do
     AccessPoints[$N]=$AP
-    N=$((N+1))
-done
+    ((N++))
+done < <(awk '/Address/ {print $5}' iwlist_example.log)
+
 N=1
-grep "Signal level" iwlist_example.log | sed -e 's/^.*Signal level=\(.*\) dBm/\1/'  | while read SL
+while read SL
 do
     SignalLevel[$N]=$SL
-    N=$((N+1))
-done
+    ((N++))
+done < <(grep "Signal level" iwlist_example.log | sed -e 's/^.*Signal level=\(.*\) dBm/\1/')
 
+for i in $(seq 1 ${#AccessPoints[@]})
+do
+    echo "Access Point: ${AccessPoints[$i]}"
+    echo "Signal Level: ${SignalLevel[$i]}"
+done
