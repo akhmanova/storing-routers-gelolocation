9ef31170bb16d94510fd9c14130cd3f6df07ab3c
akhmanova <eakhmanova@gmail.com>
2017-02-09 19:34:00 +0400

    GIT's commands changed

--- geolocation.sh
+++ geolocation.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 source vars
-
-OUR_DIR="/tmp/geolocation/$(hostname)"
+hostname = `cat /etc/hostname`
+OUR_DIR="/tmp/geolocation/$hostname"
 
 if [ ! -d ".git" ]
 then 
-    git init
-    git remote add origin https://github.com/akhmanova/storing-routers-gelolocation.git
-    git push -u origin master
+    git clone https://github.com/akhmanova/storing-routers-gelolocation.git
+    git push origin master
 fi
 
-cd $(hostname)
-wget -O "/home/eakhmanova/git_gd/storing/geolocation.json" --post-file=post_data.json --header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=$GOOGLE_KEY"
+if [ ! -d "$hostname" ]
+then 
+    mkdir $hostname
+fi
+
+cd $hostname
+wget -O "geolocation.json" --post-file=post_data.json --header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=$GOOGLE_KEY"
 
 git add geolocation.json
 
 git commit -m "Geolocation changed"
+git push origin master
 
