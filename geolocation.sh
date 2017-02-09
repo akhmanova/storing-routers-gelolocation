d720b306714d9890d8e22a236b4309b1d3d3a47e
akhmanova <eakhmanova@gmail.com>
2017-02-09 19:18:37 +0400

    dir changed

--- geolocation.sh
+++ geolocation.sh
@@ -10,6 +10,8 @@ then
     git remote add origin https://github.com/akhmanova/storing-routers-gelolocation.git
     git push -u origin master
 fi
+
+cd $(hostname)
 wget -O "/home/eakhmanova/git_gd/storing/geolocation.json" --post-file=post_data.json --header "Content-Type: application/json" "https://www.googleapis.com/geolocation/v1/geolocate?key=$GOOGLE_KEY"
 
 git add geolocation.json
