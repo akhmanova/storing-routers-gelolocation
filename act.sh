20cc34e86033c307af9a89de6671e03968ddfde6
akhmanova <eakhmanova@gmail.com>
2017-02-10 15:22:42 +0400

    {} signalToNoiseRatio changed

--- act.sh
+++ act.sh
@@ -37,9 +37,9 @@ do
     done
     echo -n " { \"macAddress\": \"${AccessPoints[$i]}\", " >> post_data.json
     echo -n " { \"Channel\": \"${Channel[$i]}\", " >> post_data.json
-    signalToNoiseRatio[$i]=`echo "$((-$SignalLevel[$i]*70))/$Quality[$i]*70" | bc`
+    signalToNoiseRatio[$i]=`echo "$((-${SignalLevel[$i]}*70))/${Quality[$i]}*70" | bc`
 
-    echo -n "\"signalStrength\": ${SignalLevel[$i]}, \"signalToNoiseRatio\": $signalToNoiseRatio[$i]}" >> post_data.json
+    echo -n "\"signalStrength\": ${SignalLevel[$i]}, \"signalToNoiseRatio\": ${signalToNoiseRatio[$i]}" >> post_data.json
 done
 
 echo -n "]}'" >> post_data.json
