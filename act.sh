3b7b9515b8e718bb8ee96f605e4fc93b1a6ae088
akhmanova <eakhmanova@gmail.com>
2017-02-10 17:31:17 +0400

    accuracy changed

--- act.sh
+++ act.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-echo -n "'{\"considerIp\": \"false\", \"wifiAccessPoints\": [" > post_data.json
+echo -n "{\"considerIp\": \"false\", \"wifiAccessPoints\": [" > post_data.json
 N=1
 while read AP
 do
-    AccessPoints[$N]=$AP
+    AccessPoints[$N]=$(echo $AP | tr "[:upper:]" "[:lower:]")
     ((N++))
 done < <(awk '/Address/ {print $5}' iwlist_example.log)
-
-N=1
-while read CN
-do
-    Channel[$N]=$CN
-    ((N++))
-done < <(grep -e "Channel:" iwlist_example.log | while read l1; do echo $l1; done  | sed -e 's/Channel://')
 
 N=1
 while read SL
@@ -20,13 +13,6 @@ do
     SignalLevel[$N]=$SL
     ((N++))
 done < <(grep "Signal level" iwlist_example.log | sed -e 's/^.*Signal level=\(.*\) dBm/\1/')
-
-N=1
-while read QL
-do
-    Quality[$N]=$QL
-    ((N++))
-done < <(grep -e "Address:" -e "level=" -e "Channel:" iwlist_example.log | while read l1 && read l2 && read l3; do echo $l1 $l2 $l3; done | awk '{print $7;}' | sed -e 's/Quality=//' | sed -e 's/\(.*\)\/.*/\1/')
 
 for i in $(seq 1 ${#AccessPoints[@]})
 do
@@ -36,12 +22,9 @@ do
         break
     done
     echo -n " { \"macAddress\": \"${AccessPoints[$i]}\", " >> post_data.json
-    echo -n "  \"Channel\": \"${Channel[$i]}\", " >> post_data.json
-    signalToNoiseRatio[$i]=`echo "$((-${SignalLevel[$i]}*70))/${Quality[$i]}*70" | bc`
-
-    echo -n "\"signalStrength\": ${SignalLevel[$i]}, \"signalToNoiseRatio\": ${signalToNoiseRatio[$i]} }" >> post_data.json
+    echo -n "\"signalStrength\": ${SignalLevel[$i]} }" >> post_data.json
 done
 
-echo -n "]}'" >> post_data.json
+echo -n "]}" >> post_data.json
 
 
